72c90046cd866e6f5b87c53741f7ed8198c97380
dapparu <dapparu@live.com>
2023-09-06 13:20:32 +0200

    update step2 file

--- step2_backgroundPrediction.C
+++ step2_backgroundPrediction.C
@@ -50,8 +50,6 @@ void step2_backgroundPrediction(){
     if(fitP==2) outfilename_ += "_fitPUp";
 
     outfilename_ += "_nPE"+to_string(nPE);
-    //outfilename_ += "_11april_wFit_ih0sigmapt0iso0";
-    //outfilename_ += "_14april_ih0sigmapt2iso1";
     outfilename_ += "_UnB_v3_Data_v2";
 
     std::cout << outfilename_ << std::endl;
@@ -61,20 +59,6 @@ void step2_backgroundPrediction(){
     TFile* ifile = new TFile((filename+".root").c_str());
 
     // histograms used for the mass prediction
-    //------------
-
-/*
-    std::string dir = "analyzer/BaseName/";
-    //std::string dir = "HSCParticleAnalyzer/BaseName/";
-    TH2F* eta_cutIndex_regA = (TH2F*)ifile->Get((dir+"Pred_EtaB").c_str())->Clone(); 
-    TH2F* eta_cutIndex_regB = (TH2F*)ifile->Get((dir+"Pred_EtaS").c_str())->Clone(); 
-    TH3F* ih_eta_cutIndex_regB = (TH3F*)ifile->Get((dir+"Pred_EtaI").c_str())->Clone(); 
-    TH3F* eta_p_cutIndex_regC = (TH3F*)ifile->Get((dir+"Pred_EtaP").c_str())->Clone(); 
-    TH1F* H_A = (TH1F*)ifile->Get((dir+"H_A").c_str())->Clone();
-    TH1F* H_B = (TH1F*)ifile->Get((dir+"H_B").c_str())->Clone();
-    TH1F* H_C = (TH1F*)ifile->Get((dir+"H_C").c_str())->Clone();
-    TH2F* mass_cutIndex = (TH2F*)ifile->Get((dir+"Mass").c_str())->Clone();*/
-
     //------------
 
     Region ra_ias50;
@@ -113,15 +97,6 @@ void step2_backgroundPrediction(){
     Region rbc_99ias100;
     Region rbc_999ias100;
 
-    /*Region ra_1o4;
-    Region ra_2o4;
-    Region ra_3o4;
-    Region ra_4o4;
-    Region rb_1o2;
-    Region rb_2o2;
-    Region rc_1o2;
-    Region rc_2o2;
-    Region rd_1o1;*/
     Region rb_true;
     Region rc_true;
 
@@ -143,10 +118,6 @@ void step2_backgroundPrediction(){
     Region rb_true_90ias100;
     Region rc_true_90ias100;
 
-    /*Region rbc_1o2_b;
-    Region rbc_2o2_b;
-    Region rbc_1o2_c;
-    Region rbc_2o2_c;*/
     Region rbc_true_b;
     Region rbc_true_c;
     
@@ -192,27 +163,6 @@ void step2_backgroundPrediction(){
     loadHistograms(rbc_99ias100,ifile,"regionD_99",bool_rebin,rebineta,rebinp,rebinih,rebinmass,"regionBC_99_100");
     loadHistograms(rbc_999ias100,ifile,"regionD_999",bool_rebin,rebineta,rebinp,rebinih,rebinmass,"regionBC_999_100");
 */
-/*    loadHistograms(ra_ias50,ifile,"regionA_ias50",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(rc_ias50,ifile,"regionC_ias50",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(rb_50ias90,ifile,"regionB_50ias90",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(rd_50ias90,ifile,"regionD_50ias90",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(rbc_50ias90,ifile,"regionD_50ias90",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-*/
-/*
-    loadHistograms(ra_ias50,ifile,"regionA_ias50_testIhPt",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(rc_ias50,ifile,"regionC_ias50_testIhPt",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    
-    loadHistograms(rb_50ias90,ifile,"regionB_50ias90_testIhPt",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(rd_50ias90,ifile,"regionD_50ias90_testIhPt",bool_rebin,rebineta,rebinp,rebinih,rebinmass);
-    loadHistograms(rbc_50ias90,ifile,"regionD_50ias90_testIhPt",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    
-    loadHistograms(rb_90ias100,ifile,"regionB_90ias100_testIhPt",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(rd_90ias100,ifile,"regionD_90ias100_testIhPt",bool_rebin,rebineta,rebinp,rebinih,rebinmass);
-    loadHistograms(rbc_90ias100,ifile,"regionD_90ias100_testIhPt",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-
-*/
-
-//comment test Raph pT>200 GeV
 
     loadHistograms(ra_ias50,ifile,"regionA_ias50",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
     loadHistograms(rc_ias50,ifile,"regionC_ias50",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
@@ -244,7 +194,6 @@ void step2_backgroundPrediction(){
     loadHistograms(rbc_99ias100,ifile,"regionD_99ias100",bool_rebin,rebineta,rebinp,rebinih,rebinmass);
     loadHistograms(rbc_999ias100,ifile,"regionD_999ias100",bool_rebin,rebineta,rebinp,rebinih,rebinmass);
 
-
     loadHistograms(rb_true,ifile,"regionD_50ias90",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
     loadHistograms(rc_true,ifile,"regionD_50ias90",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
 
@@ -265,23 +214,6 @@ void step2_backgroundPrediction(){
 
     loadHistograms(rb_true_90ias100,ifile,"regionD_90ias100",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
     loadHistograms(rc_true_90ias100,ifile,"regionD_90ias100",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-
-
-/*
-    loadHistograms(ra_1o4,ifile,"regA_1o4",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(ra_2o4,ifile,"regA_2o4",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(ra_3o4,ifile,"regA_3o4",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(ra_4o4,ifile,"regA_4o4",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(rb_1o2,ifile,"regB_1o2",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(rb_2o2,ifile,"regB_2o2",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(rc_1o2,ifile,"regC_1o2",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(rc_2o2,ifile,"regC_2o2",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(rd_1o1,ifile,"regD_1o1",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-
-    loadHistograms(rb_true,ifile,"regionD_50_90",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-    loadHistograms(rc_true,ifile,"regionD_50_90",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
-*/
-
 
 
 /*    loadHistograms(ra_ias50,ifile,"regA_ias50",bool_rebin,rebineta,rebinp,rebinih,rebinmass); 
@@ -314,12 +246,11 @@ void step2_backgroundPrediction(){
 
     TFile* ofile = new TFile((outfilename_+".root").c_str(),"RECREATE");
 
-    std::cout << "saving... " << std::endl;
+    std::cout << "background estimation... " << std::endl;
 
 
     // estimate the background in different Ias slices, each containing 10% of the statistic 
     // ------------------------------------------------------------------------------------------------------
- 
 
     //bckgEstimate(st_sample, dirname, rb_50ias60, rc_ias50, rbc_50ias60, ra_ias50, rd_50ias60, "50ias60", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, false);
     //bckgEstimate(st_sample, dirname, rb_60ias70, rc_ias50, rbc_60ias70, ra_ias50, rd_60ias70, "60ias70", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, false);
@@ -332,164 +263,7 @@ void step2_backgroundPrediction(){
     bckgEstimate(st_sample, dirname, rb_99ias100, rc_ias50, rbc_99ias100, ra_ias50, rd_99ias100, "99ias100", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, false);
     bckgEstimate(st_sample, dirname, rb_999ias100, rc_ias50, rbc_999ias100, ra_ias50, rd_999ias100, "999ias100", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, false);
 
-/*
-    vector<float> vX;
-    vector<float> vXerr;
-
-    vX.push_back(1);
-    vX.push_back(2);
-    vX.push_back(3);
-    vX.push_back(4);
-    vX.push_back(5);
-    vX.push_back(7);
-
-    vXerr.push_back(0);
-    vXerr.push_back(0);
-    vXerr.push_back(0);
-    vXerr.push_back(0);
-    vXerr.push_back(0);
-    vXerr.push_back(0);
-
-    bckgEstimate(st_sample, dirname, rb_50ias60, rc_ias50, rbc_50ias60, ra_ias50, rd_50ias60, "50ias60", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, true);
-    bckgEstimate(st_sample, dirname, rb_60ias70, rc_ias50, rbc_60ias70, ra_ias50, rd_60ias70, "60ias70", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, true);
-    bckgEstimate(st_sample, dirname, rb_70ias80, rc_ias50, rbc_70ias80, ra_ias50, rd_70ias80, "70ias80", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, true);
-    bckgEstimate(st_sample, dirname, rb_80ias90, rc_ias50, rbc_80ias90, ra_ias50, rd_80ias90, "80ias90", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, true);
-    
-    bckgEstimate(st_sample, dirname, rb_90ias100, rc_ias50, rbc_90ias100, ra_ias50, rd_90ias100, "90ias100", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, true);
-    
-    bckgEstimate(st_sample, dirname, rb_50ias90, rc_ias50, rbc_50ias90, ra_ias50, rd_50ias90, "50ias90", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, true);
-
-    //bckgEstimate(st_sample, dirname, rb_true, rc_true, rbc_50ias90, ra_ias50, rd_50ias90, "True_BC", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP);
-    for(int i=0;i<v_par0.size();i++){
-        cout << vX[i] << " " << vXerr[i] << " " << v_par0[i] << " " << v_err0[i] << " " << v_par1[i] << " " << v_err1[i] << endl;
-    }
-    TGraphErrors grPar0(vX.size(),vX.data(),v_par0.data(),vXerr.data(),v_err0.data());
-    TGraphErrors grPar1(vX.size(),vX.data(),v_par1.data(),vXerr.data(),v_err1.data());
-
-    grPar0.SetTitle("Par0");
-    grPar1.SetTitle("Par1");
-
-    char const *range[7] = {"D_{50-60}","D_{60-70}","D_{70-80}","D_{80-90}","D_{90-100}","","D_{50-90}"};
-
-    for(int i=1;i<=7;i++){
-        grPar0.GetXaxis()->SetBinLabel(i,range[i-1]);
-        grPar1.GetXaxis()->SetBinLabel(i,range[i-1]);
-    }
-
-    TCanvas c;
-    grPar0.Draw("AP*");
-    c.SaveAs((dirname+"grPar0.pdf").c_str());
-    c.SaveAs((dirname+"grPar0.root").c_str());
-    c.SaveAs((dirname+"grPar0.C").c_str());
-    grPar1.Draw("AP*");
-    c.SaveAs((dirname+"grPar1.pdf").c_str());
-    c.SaveAs((dirname+"grPar1.root").c_str());
-    c.SaveAs((dirname+"grPar1.C").c_str());
-
-    grPar0.Write();
-    grPar1.Write();
-*/
-    /*
-    vector<float> vX;
-    vector<float> vXerr;
-
-    vX.push_back(1);
-    vX.push_back(2);
-    vX.push_back(3);
-    vX.push_back(4);
-    vX.push_back(5);
-    vX.push_back(7);
-
-    vXerr.push_back(0);
-    vXerr.push_back(0);
-    vXerr.push_back(0);
-    vXerr.push_back(0);
-    vXerr.push_back(0);
-    vXerr.push_back(0);
-    
-    
-    bckgEstimate(st_sample, dirname, rb_true_60ias70, rc_true_60ias70, rbc_60ias70, ra_ias50, rd_60ias70, "True_60ias70", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, false);
-    bckgEstimate(st_sample, dirname, rb_true_50ias60, rc_true_50ias60, rbc_50ias60, ra_ias50, rd_50ias60, "True_50ias60", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, false);
-    bckgEstimate(st_sample, dirname, rb_true_70ias80, rc_true_70ias80, rbc_70ias80, ra_ias50, rd_70ias80, "True_70ias80", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, false);
-    bckgEstimate(st_sample, dirname, rb_true_80ias90, rc_true_80ias90, rbc_80ias90, ra_ias50, rd_80ias90, "True_80ias90", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, false);
-    bckgEstimate(st_sample, dirname, rb_true_90ias100, rc_true_90ias100, rbc_90ias100, ra_ias50, rd_90ias100, "True_90ias100", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, true);
-    bckgEstimate(st_sample, dirname, rb_true_50ias90, rc_true_50ias90, rbc_50ias90, ra_ias50, rd_50ias90, "True_50ias90", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, false);
-    
-
-    for(int i=0;i<v_par0.size();i++){
-        cout << vX[i] << " " << vXerr[i] << " " << v_par0[i] << " " << v_err0[i] << " " << v_par1[i] << " " << v_err1[i] << endl;
-    }
-    TGraphErrors grPar0(vX.size(),vX.data(),v_par0.data(),vXerr.data(),v_err0.data());
-    TGraphErrors grPar1(vX.size(),vX.data(),v_par1.data(),vXerr.data(),v_err1.data());
-
-    grPar0.SetTitle("Par0");
-    grPar1.SetTitle("Par1");
-
-    char const *range[7] = {"D_{50-60}","D_{60-70}","D_{70-80}","D_{80-90}","D_{90-100}","","D_{50-90}"};
-
-    for(int i=1;i<=7;i++){
-        grPar0.GetXaxis()->SetBinLabel(i,range[i-1]);
-        grPar1.GetXaxis()->SetBinLabel(i,range[i-1]);
-    }
-
-    TCanvas c;
-    grPar0.Draw("AP*");
-    c.SaveAs((dirname+"grPar0.pdf").c_str());
-    c.SaveAs((dirname+"grPar0.root").c_str());
-    c.SaveAs((dirname+"grPar0.C").c_str());
-    grPar1.Draw("AP*");
-    c.SaveAs((dirname+"grPar1.pdf").c_str());
-    c.SaveAs((dirname+"grPar1.root").c_str());
-    c.SaveAs((dirname+"grPar1.C").c_str());
-
-    grPar0.Write();
-    grPar1.Write();
-    
-    
-    
-    */
-
-    //bckgEstimate(st_sample, dirname, rd_90ias100, rd_90ias100, rbc_90ias100, ra_ias50, rd_90ias100, "True_90ias100", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP, true);
-    
-    
-    //bckgEstimate(st_sample, dirname, rb_90ias100, rc_ias50, rbc_90ias100, ra_ias50, rd_90ias100, "90ias100_nominal", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP);
-    //bckgEstimate(st_sample, dirname, rb_99ias100, rc_ias50, rbc_99ias100, ra_ias50, rd_99ias100, "99ias100_nominal", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP);
-    //bckgEstimate(st_sample, dirname, rb_999ias100, rc_ias50, rbc_999ias100, ra_ias50, rd_999ias100, "999ias100_nominal", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP);
-    
-    //bckgEstimate(st_sample, dirname, rb_90ias100, rc_ias50, rbc_90ias100, ra_ias50, rd_90ias100, "90ias100_down", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP);
-    //bckgEstimate(st_sample, dirname, rb_90ias100, rc_ias50, rbc_90ias100, ra_ias50, rd_90ias100, "90ias100_up", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP);
-    
-
-    //bckgEstimate(st_sample, dirname, rb_50ias99, rc_ias50, rbc_50ias99, ra_ias50, rd_50ias99, "50ias99", nPE);
-    //bckgEstimate(st_sample, dirname, rb_90ias100, rc_ias50, rbc_90ias100, ra_ias50, rd_90ias100, "90ias100", nPE);
-    //bckgEstimate(st_sample, dirname, rb_99ias100, rc_ias50, rbc_99ias100, ra_ias50, rd_99ias100, "99ias100", nPE);
-    //bckgEstimate(st_sample, dirname, rb_50ias999, rc_ias50, rbc_50ias999, ra_ias50, rd_50ias999, "50ias999", nPE);
-    //bckgEstimate(st_sample, dirname, rb_999ias100, rc_ias50, rbc_999ias100, ra_ias50, rd_999ias100, "999ias100", nPE);
-
-
-    //bckgEstimate(st_sample, dirname, rb_1o2, rc_1o2, rbc_50ias90, ra_1o4, rd_50ias90, "1o4", nPE);
-    //bckgEstimate(st_sample, dirname, rb_2o2, rc_1o2, rbc_50ias90, ra_2o4, rd_50ias90, "2o4", nPE);
-    //bckgEstimate(st_sample, dirname, rb_1o2, rc_2o2, rbc_50ias90, ra_3o4, rd_50ias90, "3o4", nPE);
-    //bckgEstimate(st_sample, dirname, rb_2o2, rc_2o2, rbc_50ias90, ra_4o4, rd_50ias90, "4o4", nPE);
-
-    //bckgEstimate(st_sample, dirname, rb_true, rc_ias50, rbc_50ias90, ra_ias50, rd_50ias90, "True_B", nPE);
-    //bckgEstimate(st_sample, dirname, rb_50ias90, rc_true, rbc_50ias90, ra_ias50, rd_50ias90, "True_C", nPE);
-    
-    
-    //bckgEstimate(st_sample, dirname, rb_true, rc_true, rbc_50ias90, ra_ias50, rd_50ias90, "True_BC", nPE, corrTemplateIh, corrTemplateP, fitIh, fitP);
-    
-    //bckgEstimate(st_sample, dirname, rd_50ias90, rd_50ias90, rd_50ias90, rd_50ias90, rd_50ias90, "RegionD", nPE);
-    
-    // ------------------------------------------------------------------------------------------------------
-   
-    // bkg estimate for a selected cut index 
-    // cutIndex = 3 --> pT > 60 GeV & Ias > 0.05
-   
-
-    //bckgEstimate_fromHistos(st_sample, dirname, *mass_cutIndex, *eta_cutIndex_regA, *eta_cutIndex_regB, *ih_eta_cutIndex_regB, *eta_p_cutIndex_regC, *H_A, *H_B, *H_C, cutIndex, nPE);
-
     delete ofile;
-    //delete mass_cutIndex;
 
     return;
 }
